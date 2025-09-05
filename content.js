d76432cd29d53d85b522c96b7a62bd62c95b0bc6
Clarence Jordan <clarencejordan087@gamil.com.com>
2025-09-04 21:52:00 -0500

    start btn

--- content.js
+++ content.js
@@ -1,4 +1,63 @@
+
+
+
+
+
+
+
 document.addEventListener("click", () => {
     console.log("Content script clicked!");
-    chrome.runtime.sendMessage({userInput:"clicked  !"})
+  
 });
+
+const startIndeed = (() => {
+startScriptButton()
+});
+
+function indeedMain () {
+    new Promise((resolve) => {
+        const checkExist = setInterval(() => {
+            if (document.getElementById('MosaicProviderRichSearchDaemon')) {
+                clearInterval(checkExist);
+                resolve();
+            }
+        }, 100);
+    }).then(() => {
+        startIndeed();
+    }).catch((err) => {
+        console.log(err);
+    })
+}
+
+setTimeout(() => {
+    indeedMain();
+}, 2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ const startScriptButton = ()=>{
+        const searchForm = document.getElementById('MosaicProviderRichSearchDaemon')
+    const startbtn = document.createElement('button')
+    startbtn.innerText = "Start"
+    startbtn.style.height = "30px"
+    startbtn.style.width = "60px"
+    startbtn.style.backgroundColor = "blue"
+    startbtn.style.color = "white"
+    startbtn.style.borderRadius = "5px"
+    startbtn.style.border = "none"
+    startbtn.style.cursor = "pointer"
+    startbtn.style.marginLeft = "10px"
+    startbtn.id = "startbtn"
+    searchForm.appendChild(startbtn)
+}
